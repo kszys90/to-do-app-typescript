a09ce621f9089187afc8c3d68373872dbad058a7
Krzysztof Stepien <krzysztofstepien90@gmail.com>
2024-02-18 22:57:20 +0100

    App initialized from template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import CourseGoal from './components/CourseGoal'
+
+function App() {
+  return (
+    <main>
+      <CourseGoal />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/CourseGoal.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
+/* eslint-disable react/react-in-jsx-scope */
+export default function CourseGoal() {
+    return (
+        <article>
+            <div>
+                <h2>TITLE</h2>
+                <p>DESCRIPTION</p>
+            </div>
+            <button>DELETE</button>
+        </article>
+    )
+}
